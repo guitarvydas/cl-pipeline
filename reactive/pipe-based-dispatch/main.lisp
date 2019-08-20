ef53ee36a95801b628bbaf79f49656242dcb2191
Paul Tarvydas <guitarvydas@gmail.com>
2019-08-20 10:07:21 -0400

    modification to make last commit consistent

--- reactive/pipe-based-dispatch/main.lisp
+++ reactive/pipe-based-dispatch/main.lisp
@@ -1,5 +1,5 @@
 (defun main ()
   (cl-pipeline:defpipeline
    (echo "~/quicklisp/local-projects/cl-pipeline/reactive/pipe-based-dispatch/test.txt")
-   (cat-lines)
+   (cat)
    (clgrep "second hello")))
